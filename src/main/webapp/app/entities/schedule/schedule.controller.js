e74bb346ff5a5b2cb4820e181ecd5eb25663923f
Samuel Acheampong <samuel_ach67>
2016-12-12 08:56:01 -0500

    added lecturer instances on the calendar

--- src/main/webapp/app/entities/schedule/schedule.controller.js
+++ src/main/webapp/app/entities/schedule/schedule.controller.js
@@ -5,18 +5,32 @@
         .module('schedulingProjectApp')
         .controller('ScheduleController', ScheduleController);
 
-    ScheduleController.$inject = ['$scope', '$state', 'Schedule'];
+    ScheduleController.$inject = ['$scope', '$state', 'Schedule','Lecturer'];
 
 
-    function ScheduleController ($scope, $state, Schedule) {
+    function ScheduleController ($scope, $state, Schedule,Lecturer) {
         var vm = this;
 
         vm.schedules = [];
+
+        vm.Lecturers=[];
 
 angular.element(document).ready(function () {
             $('#calendar').fullCalendar({
                     // put your options and callbacks here
                 })
         });
+
+         loadAll();
+
+                function loadAll() {
+                debugger;
+                    Lecturer.query(function(result) {
+                        vm.lecturers = result;
+                    });
+                }
+
+
+
     }
 })();
